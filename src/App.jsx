fcabcd5294f53b5c081b63503aa25ff19db11287
aaronalcuitasusjr <aaronbenjmin.alcuitas.20@usjr.edu.ph>
2022-11-09 23:15:28 +0800

    add basic layout, adding string to list and clearing list

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [input, setInput] = useState("");
+  const [list, setList] = useState([]);
+
+  return (
+    <div className="App">
+      <div className="grocery-bud">
+        <div className="title-bar">
+          <div className="title">Grocery Bud</div>
+          <div className="input-container">
+            <input
+              type="text"
+              placeholder="e.g. eggs"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            ></input>
+            <button
+              className="submit-button"
+              onClick={() => {
+                if (input) {
+                  setList([...list, input]);
+                  setInput("");
+                }
+              }}
+            >
+              Submit
+            </button>
+          </div>
+          <div className="alert-container">test</div>
+        </div>
+        <div className="list-container">
+          {list.length > 0 &&
+            list.map((item) => {
+              return <div>{item}</div>;
+            })}
+        </div>
+        <div className="clear-items-container">
+          <button className="clear-button" onClick={() => setList([])}>
+            Clear Items
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
