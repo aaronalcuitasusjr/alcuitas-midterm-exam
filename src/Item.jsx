8c8bda37b4c16ccc6a92a92cf3771a4f59dbbaa3
aaronalcuitasusjr <aaronbenjmin.alcuitas.20@usjr.edu.ph>
2022-11-10 20:09:39 +0800

    add header, list, and footer styles

--- src/Item.jsx
+++ src/Item.jsx
@@ -1,12 +1,23 @@
+import { IconContext } from "react-icons";
+import { MdEdit, MdDelete } from "react-icons/md";
+
 export default function Item({ item, editItem, deleteItem }) {
   return (
     <div className="list-item">
       <p className="item-text">{item}</p>
       <button className="edit-button" onClick={editItem}>
-        edit
+        <IconContext.Provider value={{ position: "absolute", size: "20px" }}>
+          <div>
+            <MdEdit></MdEdit>
+          </div>
+        </IconContext.Provider>
       </button>
       <button className="delete-button" onClick={deleteItem}>
-        delete
+        <IconContext.Provider value={{ position: "absolute", size: "20px" }}>
+          <div>
+            <MdDelete></MdDelete>
+          </div>
+        </IconContext.Provider>
       </button>
     </div>
   );
